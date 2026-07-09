c1b68f2c5c1a9ce86fac8422d3464946e14d5949
Dde1ta <divyanshmanhasdm2005@gmail.com>
2026-07-09 14:26:01 +0530

    Inital Commit

---
+++ test_main.py
@@ -0,0 +1,13 @@
+class TestMath:
+
+    def test_one(self):
+        case1 = "Hello"
+        assert case1 == "Hello"
+
+    def test_two(self):
+        case2 = 4
+        assert case2 % 2 == 0
+
+    def test_three(self):
+        case3 = "ThisIsAlphanumeric"
+        assert case3.isalnum()
