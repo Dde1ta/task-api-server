3332842312ee6d16a5362f63e292d80c176fe3ee
Dde1ta <divyanshmanhasdm2005@gmail.com>
2026-07-13 15:02:05 +0530

    Add maker GET call

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/maker")
+async def maker():
+    return {"name": "D_delta", "place": "India"}
